1d14bcad8266b426a9435629e8679290cd23d691
Bianca Barbosa <yume_shiro@hotmail.com>
2024-03-17 23:32:28 -0300

    feat: creating app and data

---
+++ src/App.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 50vw;
+  margin: 0 auto;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 3.12rem;
+  font-size: 48px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import { Container, Title } from "./App";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Title>Search Filter</Title>
+        <Outlet />
+      </Container>
+    </>
+  );
+}
+export default App;
