4e55a815d0d79bc4d62056c51ef6c4a4b122d7c5
Bianca Barbosa <yume_shiro@hotmail.com>
2024-03-21 12:48:45 -0300

    feat: creating searchbar and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Container, Title } from "./App";
-
 function App() {
   return (
     <>
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -4,10 +4,28 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+  const uniqueNames = [...new Set(data.map((item) => item.first_name))];
+
   return (
     <div>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={uniqueNames}
+        sx={{ width: 300 }}
+        onInputChange={(_, value) => setSearch(value)}
+        onChange={(_, value) => setSearch(value ? value.toLowerCase() : "")}
+        onFocus={() => setSearch("")}
+        renderInput={(params) => (
+          <TextField {...params} label="Pesquise por pessoas" />
+        )}
+      />
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -18,19 +36,25 @@ const Home = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((item) => (
-            <TableRow
-              key={item.first_name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {item.first_name}
-              </TableCell>
-              <TableCell align="right">{item.last_name}</TableCell>
-              <TableCell align="right">{item.email}</TableCell>
-              <TableCell align="right">{item.phone}</TableCell>
-            </TableRow>
-          ))}
+          {data
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.first_name.toLowerCase().includes(search);
+            })
+            .map((item) => (
+              <TableRow
+                key={item.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.first_name}
+                </TableCell>
+                <TableCell align="right">{item.last_name}</TableCell>
+                <TableCell align="right">{item.email}</TableCell>
+                <TableCell align="right">{item.phone}</TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </div>
